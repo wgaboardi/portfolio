0037ecabce07558a2a782753cb46bb49c93e0973
Wellington Luiz Gaboardi <wgaboardi@gmail.com>
2025-03-21 15:18:47 -0300

    chore: ajustes nas pastas do backend

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common'
 
 @Controller()
 export class AppController {
-  @Get('/ping')
+  @Get()
   ping(): string {
     return 'pong'
   }
--- backend/src/projeto/projeto.prisma.ts
+++ backend/src/projeto/projeto.prisma.ts
@@ -4,16 +4,16 @@ import { PrismaProvider } from 'src/db/prisma.provider';
 
 @Injectable()
 export class ProjetoPrisma {
-  constructor(private readonly prisma: PrismaProvider) {}
+	constructor(private readonly prisma: PrismaProvider) {}
 
-  async obterTodos(): Promise<Projeto[]> {
-    return this.prisma.projeto.findMany() as any;
-  }
+	  async obterTodos(): Promise<Projeto[]> {
+		return await this.prisma.projeto.findMany() as any;
+	  }
 
-  async obterPorId(id: number): Promise<Projeto | null> {
-    return this.prisma.projeto.findUnique({
-      where: { id } ,
-      include: { tecnologias: true },
-    }) as any
-  }
+	async obterPorId(id: number): Promise<Projeto | null> {
+		return this.prisma.projeto.findUnique({
+			where: { id },
+			include: { tecnologias: true },
+		}) as any
+	}
 }

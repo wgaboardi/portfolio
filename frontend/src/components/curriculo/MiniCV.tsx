c59071802b299d62fc461017a4316b63085ba824
Wellington L Gaboardi <49482719+wgaboardi@users.noreply.github.com>
2025-06-16 11:59:17 -0300

    (fix) Update MiniCV.tsx
    Adjusted photo position.

--- frontend/src/components/curriculo/MiniCV.tsx
+++ frontend/src/components/curriculo/MiniCV.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 export default function MiniCV() {
 	return (
-		<div className="flex-1 flex flex-col-reverse gap-6 md:flex-row lg:flex-col-reverse xl:flex-row items-center md:items-start lg:items-center xl:items-start bg-black border border-zinc-800 rounded-2xl px-6 pt-6">
-			<div className="relative min-w-72 h-64 xl:self-end">
+		<div className="flex-1 flex flex-col gap-6 md:flex-row xl:flex-row items-center md:items-start lg:items-center xl:items-start bg-black border border-zinc-800 rounded-2xl px-6 pt-6">
+			<div className="relative min-w-72 self-center h-64 xl:self-end">
 				<Image src="/minha-foto.png" alt="Foto de perfil" fill />
 			</div>
 			<div className="flex flex-col gap-5 self-center py-6 items-center  md:items-start lg:items-center xl:items-start">
@@ -20,7 +20,7 @@ export default function MiniCV() {
 				</div>
 				<p className="text-sm text-center md:text-left lg:text-center xl:text-left">
           Servidor Público Federal e Líder PMO. PMP certificado. Especialista em Governança de TI e Teleinformática/Redes de Computadores. 
-					Há 25 anos atua na área de desenvolvimento de softwares e há 15 anos como Gerente de projetos.
+					Há 25 anos atua na área de desenvolvimento de softwares e há 15 anos como Gerente de programas e projetos técnicos.
 				</p>
 			</div>
 		</div>

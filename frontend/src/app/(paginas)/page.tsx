a2bf8d35d1326bf1125640a99ccc58262d16815a
Wellington L Gaboardi <49482719+wgaboardi@users.noreply.github.com>
2025-06-16 13:17:01 -0300

    (feat) Update page.tsx each 1 hour

--- frontend/src/app/(paginas)/page.tsx
+++ frontend/src/app/(paginas)/page.tsx
@@ -5,6 +5,7 @@ import Principal from "@/components/landing/Principal"
 import Projetos from '@/components/projetos/Projetos'
 import { obterProjetos } from '@/functions/projetos'
 
+export const revalidate = 3600
 
 export default async function Home() {
 	const tecnologias = await obterTecnologias()
